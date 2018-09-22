5ab83a0f8d0673c41252fcdb9cd704e3893bfe2a
ls-age <build@ls-age.com>
2018-09-22 21:54:48 +0000

    chore(release): Add 0.1.1 release files [ci skip]

---
+++ out/index.js
@@ -0,0 +1,78 @@
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var colors = _interopDefault(require('chalk'));
+var prefix = _interopDefault(require('prefix-stream'));
+var execa = _interopDefault(require('execa'));
+
+const watching = Boolean(process.env.ROLLUP_WATCH);
+
+function runPlugin({
+  logPrefix = colors.magenta('run'),
+  command = 'node',
+  args,
+  run,
+  options = {},
+} = {}) {
+  if (!watching) { return { name: 'run' }; }
+
+  // eslint-disable-next-line no-console
+  const log = logPrefix ? console.log.bind(console, logPrefix) : console.log;
+
+  const isFileToRun = run ?
+    ({ file }) => file === run :
+    ({ format }) => format === 'cjs';
+
+  const prefixes = {
+    stdout: logPrefix ? `${logPrefix} ${colors.green('log')} ` : '',
+    stderr: logPrefix ? `${logPrefix} ${colors.red('err')} ` : '',
+  };
+
+  let runningProcess;
+
+  function stop() {
+    if (runningProcess) {
+      log('Stopping...');
+
+      runningProcess.kill();
+      runningProcess = null;
+    }
+  }
+
+  return {
+    name: 'run',
+    buildStart: stop,
+    generateBundle({ file, format }) {
+      if (isFileToRun({ file, format })) {
+        log('Starting...');
+
+        const current = execa(command, args || [file], options);
+        runningProcess = current;
+
+        const prefixed = {
+          stdout: prefix(prefixes.stdout),
+          stderr: prefix(prefixes.stderr),
+        };
+
+        prefixed.stdout.pipe(process.stdout);
+        prefixed.stderr.pipe(process.stderr);
+        runningProcess.stdout.pipe(prefixed.stdout);
+        runningProcess.stderr.pipe(prefixed.stderr);
+
+        runningProcess
+          .then(() => {
+            log('Process exitted');
+            runningProcess = null;
+          })
+          .catch(err => {
+            if (runningProcess === current) { // Prevent log on restart
+              log('Error:', err);
+            }
+          });
+      }
+    },
+  };
+}
+
+module.exports = runPlugin;
